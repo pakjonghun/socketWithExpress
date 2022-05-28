f087dea722a6cb44ac8f6f1a0f925d545bc56588
정훈박 <fireking5997@gmail.com>
2022-05-29 02:10:06 +0900

    add:websocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,33 @@
-console.log("app");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+const nickForm = document.getElementById("nickForm");
+const nickInput = document.getElementById("nickInput");
+
+nickForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = nickInput.value;
+  socket.send(JSON.stringify({ type: "nickname", payload: value }));
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = input.value;
+  socket.send(JSON.stringify({ type: "message", payload: value }));
+});
+
+socket.addEventListener("open", (s) => {
+  console.log("opened");
+});
+
+socket.addEventListener("message", (s) => {
+  const li = document.createElement("li");
+  li.innerText = s.data;
+  ul.prepend(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("closed");
+});
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
+import http from "http";
 import express from "express";
 import { resolve } from "path";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -11,6 +13,38 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(3000, () => {
-  console.log("start");
+app.get("/*", (req, res) => res.redirect("/"));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+
+  socket.send("message from s");
+
+  socket.on("close", () => {
+    console.log("disconnected");
+  });
+
+  socket.on("message", (message) => {
+    const json = JSON.parse(message);
+
+    if (json.type == "nickname") {
+      socket.id = json.payload;
+      return;
+    }
+
+    if (json.type == "message") {
+      sockets.forEach((s) => {
+        s.send(`${socket.id || "anno"}:${json.payload}`);
+      });
+      return;
+    }
+
+    sockets.forEach((s) => s.send(parsed));
+  });
 });
+
+server.listen(3000, () => console.log("start"));
